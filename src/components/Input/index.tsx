08da1226ad38f727a59bb3413bdab281dc2fe4f0
Luigi Zampieri <zampieri.luigi17@gmail.com>
2021-12-05 18:41:50 -0300

    first commit

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -6,11 +6,17 @@ import {
 } from 'react';
 
 import { useField } from '@unform/core';
-
+import { IconBaseProps } from 'react-icons';
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface InputProps {
+  name: string;
+  placeholder?:string;
+  icon?: React.ComponentType<IconBaseProps>;
+}
+
+export const Input = ({ name, icon: Icon, ...rest } : InputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -49,5 +55,3 @@ const Input = ({ name, icon: Icon, ...rest }) => {
     </Container>
   );
 };
-
-export default Input;
--- src/components/ModalAddFood/index.tsx
+++ src/components/ModalAddFood/index.tsx
@@ -1,30 +1,38 @@
-import { Component, createRef } from 'react';
+import { useRef } from 'react';
 import { FiCheckSquare } from 'react-icons/fi';
 
 import { Form } from './styles';
-import Modal from '../Modal';
-import Input from '../Input';
+import { Modal } from '../Modal';
+import { FormHandles } from '@unform/core';
+import { Input } from '../Input';
 
-class ModalAddFood extends Component {
-  constructor(props) {
-    super(props);
+interface AddFood {
+  image:string;
+  name:string;
+  price:string;
+  description:string;
+}
 
-    this.formRef = createRef();
-  }
+interface ModalAddFoodProps {
+  isOpen:boolean;
+  setIsOpen: () => void;
+  handleAddFood: (data: AddFood) => void
+}
 
-  handleSubmit = async data => {
-    const { setIsOpen, handleAddFood } = this.props;
+
+export function ModalAddFood({ isOpen, setIsOpen, handleAddFood }: ModalAddFoodProps) {
+
+  function handleSubmit(data: AddFood) {
 
     handleAddFood(data);
     setIsOpen();
   };
 
-  render() {
-    const { isOpen, setIsOpen } = this.props;
+  const formRef = useRef<FormHandles>(null);
 
     return (
       <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
-        <Form ref={this.formRef} onSubmit={this.handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Novo Prato</h1>
           <Input name="image" placeholder="Cole o link aqui" />
 
@@ -42,6 +50,3 @@ class ModalAddFood extends Component {
       </Modal>
     );
   }
-};
-
-export default ModalAddFood;
